d205b2438a204a467a93996cd39b361020f34df4
vagrant <vagrant@homestead>
2020-03-01 23:16:14 +0000

    A36

---
+++ database/migrations/2020_02_29_135348_create_clientes_tables.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientesTables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clientes', function (Blueprint $table) {
+          $table->increments('id');
+          $table->string('nombre', 32);
+          $table->string('imagen')->nullable();
+          $table->date('fecha_nacimiento');
+          $table->string('correo');
+          $table->rememberToken();
+          $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clientes');
+    }
+}
